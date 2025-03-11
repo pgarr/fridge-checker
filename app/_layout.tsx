66a6c6b3b47689941fc7690de8e82a32788766d0
pgarr <garlej.p@gmail.com>
2025-03-11 20:03:06 +0100

    tab navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,43 @@
-import { Stack } from "expo-router";
+import { Platform } from "react-native";
+import { Tabs } from "expo-router";
 import { SQLiteProvider } from "expo-sqlite";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import AntDesign from "@expo/vector-icons/AntDesign";
 import { migrateDbIfNeeded } from "@/utils/dataStorage";
 
 export default function RootLayout() {
   return (
     <SQLiteProvider databaseName="fridgeChecker.db" onInit={migrateDbIfNeeded}>
-      <Stack>
-        <Stack.Screen name="index" options={{ title: "List" }} />
-        <Stack.Screen name="about" options={{ title: "About" }} />
-      </Stack>
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarStyle: Platform.select({
+            ios: {
+              position: "absolute",
+            },
+            default: {},
+          }),
+        }}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color }) => (
+              <FontAwesome name="home" size={28} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="about"
+          options={{
+            title: "About",
+            tabBarIcon: ({ color }) => (
+              <AntDesign name="infocirlceo" size={28} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
     </SQLiteProvider>
   );
 }
